6bfbb7a95c1759ac21ffd1336f0ea19b99880e77
Lucas Bouju <lucasbouju@gmail.com>
2025-03-28 23:34:57 +0100

    now disassembling basic instruction correctly

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::ops::BitAnd;
+
+#[derive(Debug, PartialEq)]
 enum Instruction {
     MOV(Register, Register),
 }
@@ -14,14 +17,17 @@ impl Instruction {
     }
 }
 
+#[derive(Debug, PartialEq)]
 enum Register {
     A(RegisterPart),
     B(RegisterPart),
     BI,
+    BP,
     C(RegisterPart),
     D(RegisterPart),
     DI,
     SI,
+    SP,
 }
 
 impl Register {
@@ -30,14 +36,17 @@ impl Register {
             Register::A(register_part) => format!("a{}", register_part.to_asm_repr()),
             Register::B(register_part) => format!("b{}", register_part.to_asm_repr()),
             Register::BI => "bi".to_string(),
+            Register::BP => "bp".to_string(),
             Register::C(register_part) => format!("c{}", register_part.to_asm_repr()),
             Register::D(register_part) => format!("d{}", register_part.to_asm_repr()),
             Register::DI => "di".to_string(),
             Register::SI => "si".to_string(),
+            Register::SP => "sp".to_string(),
         }
     }
 }
 
+#[derive(Debug, PartialEq)]
 enum RegisterPart {
     High,
     Low,
@@ -55,12 +64,100 @@ impl RegisterPart {
     }
 }
 
-fn dissassemble_single_line(l: &[u8]) -> Instruction {
-    todo!()
+fn dissassemble(machine_code: &[u8]) -> Vec<Instruction> {
+    machine_code
+        .chunks(2)
+        .map(parse_mov)
+        .map(|o| o.unwrap())
+        .collect()
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn parse_mov(machine_code: &[u8]) -> Option<Instruction> {
+    let high_byte = machine_code[0];
+    let low_byte = machine_code[1];
+    let opcode = high_byte.bitand(0b11111100);
+    if opcode != 0b10001000 {
+        return None;
+    }
+    let d_flag = high_byte.bitand(0b00000010).eq(&2);
+    let w_flag = high_byte.bitand(0b00000001).eq(&1);
+
+    let mod_field = low_byte.bitand(0b11000000) >> 6;
+    let reg_field = low_byte.bitand(0b00111000) >> 3;
+    let rm_field = low_byte.bitand(0b00000111);
+
+    let (dest_reg_field, from_reg_field) = if d_flag {
+        (reg_field, rm_field)
+    } else {
+        (rm_field, reg_field)
+    };
+
+    Some(Instruction::MOV(
+        reg_field_to_reg(dest_reg_field, w_flag),
+        reg_field_to_reg(from_reg_field, w_flag),
+    ))
+}
+
+fn reg_field_to_reg(regfield: u8, w_flag: bool) -> Register {
+    match regfield {
+        0 => {
+            if !w_flag {
+                Register::A(RegisterPart::Low)
+            } else {
+                Register::A(RegisterPart::All)
+            }
+        }
+        1 => {
+            if !w_flag {
+                Register::C(RegisterPart::Low)
+            } else {
+                Register::C(RegisterPart::All)
+            }
+        }
+        2 => {
+            if !w_flag {
+                Register::D(RegisterPart::Low)
+            } else {
+                Register::D(RegisterPart::All)
+            }
+        }
+        3 => {
+            if !w_flag {
+                Register::B(RegisterPart::Low)
+            } else {
+                Register::B(RegisterPart::All)
+            }
+        }
+        4 => {
+            if !w_flag {
+                Register::A(RegisterPart::High)
+            } else {
+                Register::SP
+            }
+        }
+        5 => {
+            if !w_flag {
+                Register::C(RegisterPart::High)
+            } else {
+                Register::BP
+            }
+        }
+        6 => {
+            if !w_flag {
+                Register::D(RegisterPart::High)
+            } else {
+                Register::SI
+            }
+        }
+        7 => {
+            if !w_flag {
+                Register::B(RegisterPart::High)
+            } else {
+                Register::DI
+            }
+        }
+        _ => unreachable!(),
+    }
 }
 
 #[cfg(test)]
@@ -74,5 +171,17 @@ mod tests {
             Register::B(RegisterPart::All),
         );
         assert_eq!(instruction.to_asm(), "mov cx, bx".to_string());
+    }
+
+    #[test]
+    fn test_instruction_parser_basic() {
+        let machine_code = &[0b10001011_u8, 0b00001011_u8];
+        assert_eq!(
+            parse_mov(machine_code),
+            Some(Instruction::MOV(
+                Register::C(RegisterPart::All),
+                Register::B(RegisterPart::All),
+            ))
+        );
     }
 }
